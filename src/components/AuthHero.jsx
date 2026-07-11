b968d66b5ac5cc0d89a7383fda133a20af867a02
vic-ec <paulxafis@gmail.com>
2026-07-11 21:50:47 +0200

    Update AuthHero image import to new file

--- src/components/AuthHero.jsx
+++ src/components/AuthHero.jsx
@@ -1,4 +1,4 @@
-import robotLily from '../assets/lily-robot-ginger-shadow.png'
+import robotLily from '../assets/lily-robot-ginger-mirrorshadow-nobg.png'
 import butterflyImg from '../assets/butterfly-v3.png'
 import RotaCat from './RotaCat'
 

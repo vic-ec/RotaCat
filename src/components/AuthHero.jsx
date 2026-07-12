10270e86d2441630e5d47366a8241299b60a5d69
vic-ec <paulxafis@gmail.com>
2026-07-12 12:16:08 +0200

    Update image source for robotLily

--- src/components/AuthHero.jsx
+++ src/components/AuthHero.jsx
@@ -1,4 +1,4 @@
-import robotLily from '../assets/lily-robot-ginger-mirrorshadow.png'
+import robotLily from '../assets/lily-robot-ginger-mirrorshadow-2x.png'
 import butterflyImg from '../assets/butterfly-v3.png'
 import RotaCat from './RotaCat'
 

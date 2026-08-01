d2651d3551f7a2d6fabe595a0138508279aedecd
vic-ec <paulxafis@gmail.com>
2026-08-01 23:05:48 +0200

    Update AuthHero.jsx

--- src/components/AuthHero.jsx
+++ src/components/AuthHero.jsx
@@ -19,7 +19,7 @@ export default function AuthHero() {
           <img
             src={butterflyLoop}
             alt=""
-            className="absolute -top-[15.5px] -right-[12px] h-[25px] w-[25px] -rotate-8 select-none"
+            className="absolute -top-[14.5px] -right-[12.5px] h-[25px] w-[25px] -rotate-8 select-none"
             draggable="false"
           />
         </span>
